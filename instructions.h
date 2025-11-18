53219ac9641316d0d7ff775aa49008496d8c88dd
pstewart1244 <p.stewart1244@gmail.com>
2025-11-18 10:31:18 -0800

    Added directives

--- instructions.h
+++ instructions.h
@@ -1,5 +1,6 @@
 #define MNEMONIC_MAX_LEN    6
 #define NUM_INSTRUCTIONS    59
+#define NUM_DIRECTIVES      14
 
 // Bitmaps for format flags
 #define FMT1 0b1000
@@ -7,6 +8,8 @@
 #define FMT3 0b0010
 #define FMT4 0b0001
 
+//Instruction type definition
+typedef struct{
 struct Instruction_Struct{
     char mnemonic[MNEMONIC_MAX_LEN + 1];
     unsigned int formats;   //0b[1][2][3][4] T/F flag for formats
@@ -18,6 +21,29 @@ struct Directive_Struct{
 
 };
 
+//Assembler Directive list:
+Directive directives[] = {
+    {"START" },
+    {"END"   },
+    {"RESB"  },
+    {"RESW"  },
+    {"BYTE"  },
+    {"WORD"  },
+    {"BASE"  },
+    {"NOBASE"},
+    {"*"     },
+    {"LTORG" },
+    {"USE"   },    
+    {"CSECT" },
+    {"EXTDEF"},
+    {"EXTREF"},
+};
+
+
+//Search methods (recognizes instruction/directive input)
+Instruction findInstruction(char *mnemonic);
+
+Directive findDirective(char *mnemonic);
 typedef struct Instruction_Struct Instruction;
 typedef struct Directive_Struct Directive;
 
