c70bade0521af636a18cfe12374cd9dcf0584141
Sanmitr564 <generic39542@gmail.com>
2025-11-20 21:23:04 -0800

    Add names, redid, edoras to files

--- assembler.c
+++ assembler.c
@@ -1,3 +1,9 @@
+/*
+Aaron Alegre,       cssc2101, Red ID: 828222103
+Patrick Stewart,    cssc2138, Red ID: 130969511
+Edwin Vega,         cssc2141, Red ID: 827746186
+*/
+
 #define _CRT_SECURE_NO_WARNINGS
 
 #ifndef POSIX_COMPAT_H
--- instructions.h
+++ instructions.h
@@ -1,3 +1,9 @@
+/*
+Aaron Alegre,       cssc2101, Red ID: 828222103
+Patrick Stewart,    cssc2138, Red ID: 130969511
+Edwin Vega,         cssc2141, Red ID: 827746186
+*/
+
 #define MNEMONIC_MAX_LEN    6
 #define NUM_INSTRUCTIONS    59
 #define NUM_DIRECTIVES      14
